6f83dc958fe53b1a1170fde0224a4f7540bce60f
DavidSheridan <dpmsheridan@hotmail.com>
2018-10-11 12:26:59 +1300

    Implemented the SyntacticElement class.

--- src/main/java/pmc/util/SyntacticElement.java
+++ src/main/java/pmc/util/SyntacticElement.java
@@ -6,4 +6,34 @@ package pmc.util;
  */
 public abstract class SyntacticElement {
 
+    // field
+    private Position position;
+
+    /**
+     * Constructs a new instance of a {@code SyntacticElement} object with the
+     * position pointing to the beginning of an input stream.
+     */
+    public SyntacticElement(){
+        position = new Position(0, 0, 0, 0);
+    }
+
+    /**
+     * Constructs a new instance of a {@code SyntacticElement} object with the
+     * specified {@code Position}.
+     *
+     * @param position The position of this {@code SyntacticElement}.
+     */
+    public SyntacticElement(Position position){
+        this.position = position;
+    }
+
+    /**
+     * Returns the position of this {@code SyntacticElement}.
+     *
+     * @return The position of this {@code SyntacticElement}.
+     */
+    public Position getPosition(){
+        return position;
+    }
+
 }
