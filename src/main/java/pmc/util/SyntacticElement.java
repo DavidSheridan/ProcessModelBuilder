820293908ec32eae33bcf24e53d2276584041f90
DavidSheridan <dpmsheridan@hotmail.com>
2018-10-11 12:17:21 +1300

    Initialised the SyntacticElement class for representing the position of elements in an input stream.

---
+++ src/main/java/pmc/util/SyntacticElement.java
@@ -0,0 +1,9 @@
+package pmc.util;
+
+/**
+ * The {@code SyntacticElement} class represents the position of a particular element
+ * in an input stream.
+ */
+public abstract class SyntacticElement {
+
+}
