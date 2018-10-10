ebfbc6cb911b9deb11f749e1e06efe9efff42086
DavidSheridan <dpmsheridan@hotmail.com>
2018-10-11 11:41:15 +1300

    Implemented an interface for representing terminals found in the Process Modeller grammar.

---
+++ src/main/java/pmc/lang/Terminal.java
@@ -0,0 +1,17 @@
+package pmc.lang;
+
+/**
+ * The {@code Terminal} interface is a superclass for all terminals in the Process
+ * Modeller grammar.
+ */
+public interface Terminal {
+
+    /**
+     * Returns a {@code String} representation of this {@code Terminal} as it
+     * is defined by the Process Modeller grammar.
+     *
+     * @return The {@code String} representation
+     */
+    String getValue();
+
+}
