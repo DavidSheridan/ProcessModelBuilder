43efa8aa2dd8436e04f6e50a6a0137dbf98e3f40
DavidSheridan <dpmsheridan@hotmail.com>
2018-10-11 11:53:18 +1300

    Implemented an enum for representing the terminator types found in the Process Modeller grammar.

---
+++ src/main/java/pmc/lang/TerminatorType.java
@@ -0,0 +1,18 @@
+package pmc.lang;
+
+/**
+ * An enum representing the terminator types found in the Process Modeller grammar.
+ */
+public enum TerminatorType implements Terminal {
+    STOP;
+
+    /**
+     * Returns the {@code String} representation of this {@code TerminatorType} as it is
+     * defined in the Process Modeller grammar.
+     *
+     * @return The {@code String} representation.
+     */
+    public String getValue(){
+        return this.name();
+    }
+}
