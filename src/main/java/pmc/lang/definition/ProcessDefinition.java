754ffad61978412b0d3795dafa60f9941163bfaf
DavidSheridan <dpmsheridan@hotmail.com>
2018-11-17 15:36:27 +1300

    Added documentation to the process definition class.

--- src/main/java/pmc/lang/definition/ProcessDefinition.java
+++ src/main/java/pmc/lang/definition/ProcessDefinition.java
@@ -11,12 +11,29 @@ public class ProcessDefinition extends SyntacticElement implements Definition {
     private String identifier;
     private Process process;
 
+    /**
+     * Constructs a new instance of a {@code ProcessDefinition} object with the specified
+     * {@code ProcessType}, {@code String} identifier and {@code Process}.
+     *
+     * @param processType The process type.
+     * @param identifier The process identifier.
+     * @param process The process.
+     */
     public ProcessDefinition(ProcessType processType, String identifier, Process process){
         this.processType = processType;
         this.identifier = identifier;
         this.process = process;
     }
 
+    /**
+     * Constructs a new instance of a {@code ProcessDefinition} object with the specified
+     * {@code ProcessType}, {@code String} identifier, {@code Process} and {@code Position}.
+     *
+     * @param processType The process type.
+     * @param identifier The process identifier.
+     * @param process The process.
+     * @param position The position of the process definition in an input stream.
+     */
     public ProcessDefinition(ProcessType processType, String identifier, Process process, Position position){
         super(position);
         this.processType = processType;
@@ -24,18 +41,39 @@ public class ProcessDefinition extends SyntacticElement implements Definition {
         this.process = process;
     }
 
+    /**
+     * Returns the {@code ProcessType} associated with this {@code ProcessDefinition}.
+     *
+     * @return The process type.
+     */
     public ProcessType getProcessType(){
         return processType;
     }
 
+    /**
+     * Returns the identifier associated with this {@code ProcessDefinition}.
+     * @return
+     */
     public String getIdentifier(){
         return identifier;
     }
 
+    /**
+     * Returns the {@code Process} associated with this {@code ProcessDefinition}.
+     * @return
+     */
     public Process getProcess(){
         return process;
     }
 
+    /**
+     * Compares this {@code ProcessDefinition} to the specified object. Returns {@code true}
+     * if and only if the argument is not {@code null} and is a {@code ProcessDefinition} object
+     * with the same {@code ProcessType}, {@code String} identifier and {@code Process}.
+     *
+     * @param obj The object to compare this {@code ProcessDefinition} against.
+     * @return {@code true} if the specified object is equivalent to this one, otherwise {@code false}.
+     */
     @Override
     public boolean equals(Object obj){
         if(obj == this){
@@ -60,6 +98,11 @@ public class ProcessDefinition extends SyntacticElement implements Definition {
         return false;
     }
 
+    /**
+     * Returns a {@code String} representation of this {@code ProcessDefinition}.
+     *
+     * @return a {@code String} representation of this {@code ProcessDefinition}.
+     */
     @Override
     public String toString(){
         return processType + " " + identifier + " = " + process + ".";
