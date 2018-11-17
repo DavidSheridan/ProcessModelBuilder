05e78a61822cc08ee8d2bdee23fcaa343a8790f2
DavidSheridan <dpmsheridan@hotmail.com>
2018-11-17 15:26:24 +1300

    Added documentation to the terminator class.

--- src/main/java/pmc/lang/process/Terminator.java
+++ src/main/java/pmc/lang/process/Terminator.java
@@ -9,19 +9,44 @@ public class Terminator extends SyntacticElement implements Process {
     // field
     private TerminatorType terminator;
 
+    /**
+     * Constructs a new instance of a {@code Terminator} with the specified {@code TerminatorType}.
+     *
+     * @param terminator The terminator type.
+     */
     public Terminator(TerminatorType terminator){
         this.terminator = terminator;
     }
 
+    /**
+     * Constructs a new instance of a {@code Terminator} with the specified {@code TerminatorType}
+     * and {@code Position}.
+     *
+     * @param terminator
+     * @param position
+     */
     public Terminator(TerminatorType terminator, Position position){
         super(position);
         this.terminator = terminator;
     }
 
+    /**
+     * Returns the {@code TerminatorType} associated with this {@code Terminator}.
+     *
+     * @return The terminator type.
+     */
     public TerminatorType getTerminator(){
         return terminator;
     }
 
+    /**
+     * Compares this {@code Terminator} to the specified object. Returns {@code true}
+     * if and only if the argument is not {@code null} and is a {@code Terminator} object
+     * with the same {@code TerminatorType} value.
+     *
+     * @param obj The object to compare this {@code Terminator} against.
+     * @return {@code true} if the specified object is equivalent to this one, otherwise {@code false}.
+     */
     @Override
     public boolean equals(Object obj){
         if(obj == this){
@@ -37,6 +62,11 @@ public class Terminator extends SyntacticElement implements Process {
         return false;
     }
 
+    /**
+     * Returns a {@code String} representation of this {@code Terminator}.
+     *
+     * @return a {@code String} representation of this {@code Terminator}.
+     */
     @Override
     public String toString(){
         return terminator.toString();
