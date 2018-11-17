2df2c6ac2d74e70375c63fea47290b07ebaaee5c
DavidSheridan <dpmsheridan@hotmail.com>
2018-11-17 15:08:03 +1300

    Added comments to the lexer test processor class.

--- src/test/java/test/processor/LexerTestProcessor.java
+++ src/test/java/test/processor/LexerTestProcessor.java
@@ -7,6 +7,13 @@ import java.util.List;
 
 public class LexerTestProcessor implements TestProcessor<List<Token>> {
 
+    /**
+     * Takes the specified {@code String} input and returns the expected {@code List}
+     * of {@code Token}s as an output.
+     *
+     * @param input The input to be processed.
+     * @return The resulting {@code List} of {@code Token}s.
+     */
     @Override
     public List<Token> process(String input){
         return new Lexer(input).scan();
