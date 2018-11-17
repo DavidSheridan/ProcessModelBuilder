cbc18840a0bb5475509bf73622b22e05889e0a71
DavidSheridan <dpmsheridan@hotmail.com>
2018-11-18 12:09:44 +1300

    Added the toActionString method declaration to the action interface.

--- src/main/java/pmc/lang/action/Action.java
+++ src/main/java/pmc/lang/action/Action.java
@@ -2,4 +2,12 @@ package pmc.lang.action;
 
 public interface Action {
 
+    /**
+     * Returns a {@code String} representation of this {@code Action} that
+     * is a valid representation of an {@code Action} within the Process Modeller grammar.
+     *
+     * @return A valid {@code String} representation of this {@code Action}.
+     */
+    String toActionString();
+
 }
