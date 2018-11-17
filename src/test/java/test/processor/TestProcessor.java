bcbf7896d2b64ce17c7ef7c81b41654d3bcb7d94
DavidSheridan <dpmsheridan@hotmail.com>
2018-11-17 15:06:23 +1300

    Added comments to the test processor interface.

--- src/test/java/test/processor/TestProcessor.java
+++ src/test/java/test/processor/TestProcessor.java
@@ -2,8 +2,17 @@ package test.processor;
 
 public interface TestProcessor<ExpectedType> {
 
+    /**
+     * A static reference to an instance of a {@code LexerTestProcessor} object.
+     */
     LexerTestProcessor LEXER = new LexerTestProcessor();
 
+    /**
+     * Takes the specified {@code String} input and returns the {@code ExpectedType} output.
+     *
+     * @param input The input to be processed.
+     * @return The resulting output.
+     */
     ExpectedType process(String input);
 
 }
