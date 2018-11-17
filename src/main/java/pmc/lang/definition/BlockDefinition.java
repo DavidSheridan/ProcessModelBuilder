9801c54d824f42e05fb4c6de4d98f6dd5c0740a5
DavidSheridan <dpmsheridan@hotmail.com>
2018-11-17 16:43:12 +1300

    Added documentation to the block definition class.

--- src/main/java/pmc/lang/definition/BlockDefinition.java
+++ src/main/java/pmc/lang/definition/BlockDefinition.java
@@ -14,23 +14,57 @@ public class BlockDefinition extends SyntacticElement implements Definition, Ite
     // field
     private List<Definition> definitions;
 
+    /**
+     * Constructs a new instance of a {@code BlockDefinition} object with the specified
+     * array of {@code Definition} objects.
+     *
+     * @param definitions The definition array.
+     */
     public BlockDefinition(Definition...definitions){
         this.definitions = Arrays.stream(definitions).collect(Collectors.toList());
     }
 
+    /**
+     * Constructs a new instance of a {@code BlockDefinition} object with the specified
+     * {@code List} of {@code Definition} objects.
+     *
+     * @param definitions {@code List} of {@code Definition} objects.
+     */
     public BlockDefinition(List<Definition> definitions){
         this.definitions = new ArrayList<Definition>(definitions);
     }
 
+    /**
+     * Constructs a new instance of a {@code BlockDefinition} object with the specified
+     * {@code List} of {@code Definition} objects and {@code Position}.
+     *
+     * @param definitions {@code List} of {@code Definition} objects.
+     * @param position The position of this {@code BlockDefinition} in an input stream.
+     */
     public BlockDefinition(List<Definition> definitions, Position position){
         super(position);
         this.definitions = new ArrayList<Definition>(definitions);
     }
 
+    /**
+     * Returns an iterator over the {@code Definition} objects in this {@code BlockDefinition} in
+     * proper sequence.
+     *
+     * @return An iterator over the {@code Definition} objects in this {@code BlockDefinition}.
+     */
+    @Override
     public Iterator<Definition> iterator(){
         return definitions.iterator();
     }
 
+    /**
+     * Compares this {@code BlockDefinition} to the specified object. Returns {@code true}
+     * if and only if the argument is not {@code null} and is a {@code BlockDefinition} object
+     * with an equivalent {@code List} of {@code Definition} objects.
+     *
+     * @param obj The object to compare this {@code BlockDefinition} against.
+     * @return {@code true} if the specified object is equivalent to this one, otherwise {@code false}.
+     */
     @Override
     public boolean equals(Object obj){
         if(obj == this){
@@ -46,6 +80,11 @@ public class BlockDefinition extends SyntacticElement implements Definition, Ite
         return false;
     }
 
+    /**
+     * Returns a {@code String} representation of this {@code BlockDefinition}.
+     *
+     * @return a {@code String} representation of this {@code BlockDefinition}.
+     */
     @Override
     public String toString(){
         return definitions.toString();
