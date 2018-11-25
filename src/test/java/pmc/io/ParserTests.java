da90925f4731468c9cc0058248bf8d0cc49367aa
DavidSheridan <dpmsheridan@hotmail.com>
2018-11-25 18:52:21 +1300

    Implemented reference tests for the parser.

--- src/test/java/pmc/io/ParserTests.java
+++ src/test/java/pmc/io/ParserTests.java
@@ -6,18 +6,23 @@ import pmc.lang.AST;
 import pmc.lang.action.ActionElementList;
 import pmc.lang.action.element.StringActionElement;
 import pmc.lang.definition.BlockDefinition;
+import pmc.lang.definition.Definition;
 import pmc.lang.definition.ProcessDefinition;
 import pmc.lang.process.Process;
+import pmc.lang.process.Reference;
 import pmc.lang.process.Sequence;
 import pmc.lang.process.Terminator;
 import pmc.lang.terminal.ProcessType;
 import pmc.lang.terminal.TerminatorType;
 import test.generator.ExampleTests;
+import test.generator.ReferenceTests;
 import test.generator.SequenceTests;
 import test.generator.TerminatorTests;
 import test.processor.TestProcessor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @DisplayName("parser tests")
 public class ParserTests {
@@ -111,6 +116,53 @@ public class ParserTests {
             return expectedWithoutParentheses(data);
         }
 
+    }
+
+    @Nested
+    @DisplayName("reference tests")
+    public class ParserReferenceTests extends ReferenceTests<AST> {
+
+        public ParserReferenceTests(){
+            super(TestProcessor.PARSER);
+        }
+
+        @Override
+        public AST sequentialExpectedWithoutParentheses(SequentialReferenceTests.TestData data){
+            List<Definition> definitions = new ArrayList<Definition>();
+            for(int i = data.actions.length - 1; i >= 0; i--){
+                Process process = (i == data.actions.length - 1) ? new Terminator(data.terminator) : new Reference(data.actions[i + 1].toUpperCase());
+                process = new Sequence(new ActionElementList(Arrays.asList(new StringActionElement(data.actions[i]))), process);
+
+                definitions.add(new ProcessDefinition(data.processType, data.actions[i].toUpperCase(), process));
+            }
+
+            return new AST(new BlockDefinition(definitions));
+        }
+
+        @Override
+        public AST sequentialExpectedWithParentheses(SequentialReferenceTests.TestData data){
+            return sequentialExpectedWithoutParentheses(data);
+        }
+
+        @Override
+        public AST selfReferenceExpectedWithoutParentheses(SelfReferenceTests.TestData data){
+            Process process = new Reference(data.identifier);
+            for(int i = data.actions.length - 1; i >= 0; i--){
+                process = new Sequence(new ActionElementList(Arrays.asList(new StringActionElement(data.actions[i]))), process);
+            }
+
+            return new AST(new BlockDefinition(new ProcessDefinition(data.processType, data.identifier, process)));
+        }
+
+        @Override
+        public AST selfReferenceExpectedWithParentheses(SelfReferenceTests.TestData data){
+            return selfReferenceExpectedWithoutParentheses(data);
+        }
+
+        @Override
+        public AST selfReferenceExpectedWithNesting(SelfReferenceTests.TestData data){
+            return selfReferenceExpectedWithoutParentheses(data);
+        }
     }
 
     @Nested
